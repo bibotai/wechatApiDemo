63bea26e88ef1ae55b301266c335374a56ace495
Fwind <ff1990@126.com>
2016-05-25 13:18:59 +0800

    index

---
+++ Demo/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/5/23.
+ */
+module.exports = {
+    appid: 'wx977151a986b5c733',
+    appsecret: 'be23fc5902b68de0093f8dd5c344a9b9'
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/5/23.
+ */
+module.exports = {
+    weixintools: require('./api/WeixinTools'),
+    usertools: require('./api/UserTools'),
+    basetools: require('./api/BaseTools'),
+    bundtools: require('./api/Bund'),
+}
