75c954c98392c9ceb0308a5a25944716cf05c7b2
Fwind <ff1990@126.com>
2016-05-25 14:57:31 +0800

    就是随便改改

--- Demo/example.js
+++ Demo/example.js
@@ -10,37 +10,37 @@ var api = new wechat(config.appid, config.appsecret);
 //获取token
 api.GetAccessToken(function (data) {
    console.log('access_token:' + JSON.parse(data)['access_token']);
-})
+});
 
-//////从cache获取token
-////api.GetAccessTokenInCache(function (data) {
-////    console.log('access_token in cache:' + data);
-////})
-//
-////获取用户信息参数
-//var argsUser = {
-//    access_token: 'access_token',
-//    openid: 'openid',
-//    lang: 'lang',
-//}
-//
-///**
-// *获取用户信息
-// */
-//wechatUesr.GetUserInfo(argsUser, function (data) {
-//    console.log('userinfo:' + data);
-//});
+////从cache获取token
+//api.GetAccessTokenInCache(function (data) {
+//    console.log('access_token in cache:' + data);
+//})
 
-// //绑定参数
-// var argsBund = {
-//     access_token: 'access_token',
-//     ticket: 'ticket',
-//     device_id:'device_id',
-//     openid: 'openid',
-// }
-// /**
-//  * 绑定成功通知
-//  */
-// wechatBund.Bund(argsBund, function (data) {
-//     console.log('bund:' + data);
-// })
+//获取用户信息参数
+var argsUser = {
+    access_token: 'access_token',
+    openid: 'openid',
+    lang: 'lang'
+};
+
+/**
+ *获取用户信息
+ */
+wechatUesr.GetUserInfo(argsUser, function (data) {
+    console.log('userinfo:' + data);
+});
+
+ //绑定参数
+ var argsBund = {
+     access_token: 'access_token',
+     ticket: 'ticket',
+     device_id:'device_id',
+     openid: 'openid'
+ };
+ /**
+  * 绑定成功通知
+  */
+ wechatBund.Bund(argsBund, function (data) {
+     console.log('bund:' + data);
+ });
