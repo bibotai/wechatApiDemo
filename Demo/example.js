7a63ac1d23deda0d83844ef75dcbb536a2d54081
bibotai <bibotai@msn.cn>
2016-05-25 14:02:53 +0800

    测试

--- Demo/example.js
+++ Demo/example.js
@@ -6,12 +6,12 @@ var wechatUesr = require('./../index').usertools;
 var wechatBund = require('./../index').bundtools;
 var config = require('./config');
 
-//var api = new wechat(config.appid, config.appsecret);
-////获取token
-//api.GetAccessToken(function (data) {
-//    console.log('access_token:' + JSON.parse(data)['access_token']);
-//})
-//
+var api = new wechat(config.appid, config.appsecret);
+//获取token
+api.GetAccessToken(function (data) {
+   console.log('access_token:' + JSON.parse(data)['access_token']);
+})
+
 //////从cache获取token
 ////api.GetAccessTokenInCache(function (data) {
 ////    console.log('access_token in cache:' + data);
@@ -31,16 +31,16 @@ var config = require('./config');
 //    console.log('userinfo:' + data);
 //});
 
-//绑定参数
-var argsBund = {
-    access_token: 'access_token',
-    ticket: 'ticket',
-    device_id:'device_id',
-    openid: 'openid',
-}
-/**
- * 绑定成功通知
- */
-wechatBund.Bund(argsBund, function (data) {
-    console.log('bund:' + data);
-})
+// //绑定参数
+// var argsBund = {
+//     access_token: 'access_token',
+//     ticket: 'ticket',
+//     device_id:'device_id',
+//     openid: 'openid',
+// }
+// /**
+//  * 绑定成功通知
+//  */
+// wechatBund.Bund(argsBund, function (data) {
+//     console.log('bund:' + data);
+// })
